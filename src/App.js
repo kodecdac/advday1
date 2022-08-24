1f365702e387844f9a4c510090f55cbe025507b8
santoshmondal <santosh.ece06@gmail.com>
2022-08-24 11:17:47 +0530

    calling the function with parameter, on button click event.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ function App() {
   let username = "cdac kharghar";
   let id = "id2";
 
+  let sayHello = (e) => {
+    console.log(e);
+  };
+
+  let helloAgain = (p1, p2) => {
+    console.log(p1, p2);
+  };
+
   return (
     <div>
       <h1>Interplation Demo</h1>
-      <h1> {username} </h1>
-
-      <h1 id="id1">{username}</h1>
-
-      {/* ATTRIBUTE LEVEL */}
       <h1 id={id}>{username}</h1>
+
+      <input type="button" value="Purchase Item" onClick={sayHello} />
+      <input type="button" value="working with param" onClick={helloAgain} />
+      <input
+        type="button"
+        value="working with param demo"
+        onClick={() => helloAgain("delhi", "calcutta")}
+      />
     </div>
   );
 }
