22b513f5e726cd06111855d2800cb86d750a5eb3
santoshmondal <santosh.ece06@gmail.com>
2022-08-24 11:02:35 +0530

    interploiation in attribute.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 // Basic Interpolation
 function App() {
   let username = "cdac kharghar";
+  let id = "id2";
   let price = 99.99;
   let active = true;
-
-  let ref = { id: 1, username: "rahul" };
-  let list = ["delhi", "calcutta"];
-  let list1 = [{ id: 1, username: "rohit" }];
-
-  // Arrow Function :: Equivalent Lamda Function
-  let sum = () => {
-    console.log("I am sum function");
-  };
 
   return (
     <div>
       <h1>Interplation Demo</h1>
       <h1> {username} </h1>
-      <h1>{price}</h1>
-      <h1>{active}</h1>
 
-      <h1>{ref.username}</h1>
-      <h1>{list}</h1>
-      <h1>{JSON.stringify(list1)}</h1>
+      <h1 id="id1">{username}</h1>
 
-      <h1>{sum()}</h1>
+      {/* ATTRIBUTE LEVEL */}
+      <h1 id={id}>{username}</h1>
     </div>
   );
 }
-
-// REVISE LIST / FUNCTION
 
 export default App;
