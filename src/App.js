5ec105b6d98388eaed3ecd00890a98c4549ba1ba
santoshmondal <santosh.ece06@gmail.com>
2022-08-25 10:42:52 +0530

    brief bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
+  let mytheme = "bg-secondary text-light p-3";
+  let darktheme = false;
   return (
     <div>
-      <h1 className="bg-primary text-light p-3">Hello World</h1>
+      <h1
+        className={
+          darktheme ? "bg-dark text-light p-2" : "bg-primary text-light p-2"
+        }
+      >
+        Interpolation CSS
+      </h1>
+
+      <hr />
+      <h1 className={mytheme}>Interpolation CSS</h1>
+
+      <hr />
+      <h1 className="bg-secondary text-light p-3">Hello World</h1>
     </div>
   );
 }
