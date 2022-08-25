46cddd4e87ce6d4bba3d8e0ceeeda143a18baa34
santoshmondal <santosh.ece06@gmail.com>
2022-08-25 10:24:54 +0530

    working with styling.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import "./App.css";
+
+// CSS using JS ?
+// <h1 style="color:red">
 function App() {
+  let theme = "orange";
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="beautify">CLASS BASED STYLING</h1>
+
+      <hr />
+      <h1 style={{ color: theme }}>Hello Universe</h1>
+      <h1 style={{ color: "red" }}>Hello World</h1>
     </div>
   );
 }
