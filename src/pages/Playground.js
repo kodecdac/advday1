98a62b9c0a35b7be5cd821b23c97b6c8472472ed
santoshmondal <santosh.ece06@gmail.com>
2022-08-29 11:44:22 +0530

    single handler. in playground.

--- src/pages/Playground.js
+++ src/pages/Playground.js
@@ -3,11 +3,117 @@ import { useState } from "react";
 function Playground() {
   return (
     <div>
+      <Playground6 />
       <Playground5 />
       <Playground4 />
       <Playground3 />
       <Playground2 />
       <Playground1 />
+    </div>
+  );
+}
+
+function Playground6() {
+  let [userList, setUserList] = useState([]);
+
+  let [user, setUser] = useState({
+    username: "",
+    password: "",
+    email: "",
+    mobile: "",
+  });
+
+  let handleInput = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSumit = () => {
+    let list1 = [user, ...userList]; // cloning.
+    setUserList(list1);
+    setUser({
+      username: "",
+      password: "",
+      email: "",
+      mobile: "",
+    });
+  };
+
+  return (
+    <div>
+      <h1>Playground 6 / with multiple Inputs / with Minimum Variables</h1>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter username"
+          name="username"
+          value={user.username}
+          onChange={handleInput}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          name="password"
+          value={user.password}
+          onChange={handleInput}
+        />
+        <input
+          type="email"
+          placeholder="Enter Email"
+          name="email"
+          value={user.email}
+          onChange={handleInput}
+        />
+        <input
+          type="mobile"
+          placeholder="Enter Mobile"
+          name="mobile"
+          value={user.mobile}
+          onChange={handleInput}
+        />
+        <input type="button" value="Submit" onClick={handleSumit} />
+      </div>
+
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">UserName</th>
+              <th scope="col">Password</th>
+              <th scope="col">Email</th>
+              <th scope="col">Mobile</th>
+              <th scope="col">Actoins</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userList.map((item, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{item.username}</td>
+                <td>******</td>
+                <td>{item.email}</td>
+                <td>{item.mobile}</td>
+                <td>
+                  <input className="btn btn-link" type="button" value="View" />{" "}
+                  /
+                  <input className="btn btn-link" type="button" value="Edit" />
+                  /<input className="btn btn-link" type="button" value="Del" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/** LIST FORM */}
+        <ul>
+          {userList.map((item) => (
+            <li>
+              {item.username} / {"****"} / {item.email} / {item.mobile}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
