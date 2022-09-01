3f5e4d5cd665366c9245d5b5df1be975fb5df8ad
santoshmondal <santosh.ece06@gmail.com>
2022-09-01 19:42:26 +0530

    t7

--- src/App.js
+++ src/App.js
@@ -1,137 +1,76 @@
-import { useDispatch, useSelector } from "react-redux";
 import {
+  Link,
   Navigate,
   NavLink,
+  Outlet,
   Route,
   Routes,
   useNavigate,
 } from "react-router-dom";
-import { login, logout } from "./store/store";
 
 function App() {
   return (
     <>
       <AppNavigation />
-
       <Routes>
-        <Route path="/" element={<Page1 />} />
+        <Route path="/" element={<main>Page1</main>} />
+        <Route path="/page1" element={<main>Page1</main>} />
+        <Route path="/page2" element={<main>Page2</main>} />
+        <Route path="/page3" element={<main>Page3</main>} />
+        <Route path="/page4" element={<main>Page4</main>} />
         <Route
-          path="/page1"
+          path="/projects"
           element={
-            <UnProtectedRoute>
-              <Page1 />
-            </UnProtectedRoute>
+            <>
+              <nav>
+                <NavLink to="/projects/dashboard">Dashboard | </NavLink>
+                <NavLink to="/projects/billing">Billing</NavLink>
+              </nav>
+              <main>
+                <section>Project Info</section>
+                <section>
+                  <Outlet />
+                </section>
+              </main>
+            </>
           }
-        />
-        <Route path="/page2" element={<Page2 />} />
-        <Route
-          path="/page3"
-          element={
-            <ProtectedRoute>
-              <Page3 />
-            </ProtectedRoute>
-          }
-        />
+        >
+          <Route index element={<main>Project Dashboard</main>} />
+          <Route path="dashboard" element={<main>Project Dashboard</main>} />
+          <Route path="billing" element={<main>Project Billing</main>} />
+        </Route>
       </Routes>
     </>
   );
 }
 
-const UnProtectedRoute = ({ children }) => {
-  let state = useSelector((state) => state);
-
-  if (state.authstore.value) {
-    return <Navigate to="/page3" replace={true}></Navigate>;
-  }
-
-  return children;
-};
-
-const ProtectedRoute = ({ children }) => {
-  let state = useSelector((state) => state);
-
-  if (!state.authstore.value) {
-    return <Navigate to="/page1" replace={true}></Navigate>;
-  }
-
-  return children;
-};
-
-const Page1 = () => {
-  const dispatch = useDispatch();
-  const click2Login = () => {
-    dispatch(login());
-  };
-  return (
-    <>
-      <h1>Page1</h1>
-      <input type="button" value="Click2Login" onClick={click2Login} />
-    </>
-  );
-};
-
-const Page2 = () => {
-  return (
-    <>
-      <h1>Page2</h1>
-    </>
-  );
-};
-
-const Page3 = () => {
-  const dispatch = useDispatch();
-  const click2Logout = () => {
-    dispatch(logout());
-  };
-
-  return (
-    <>
-      <h1>Page3</h1>
-      <input type="button" value="Click2Logout" onClick={click2Logout} />
-    </>
-  );
-};
-
 const AppNavigation = () => {
-  let state = useSelector((state) => state);
+  return (
+    <>
+      <NavLink to="/page1">Page1 |</NavLink>
+      <NavLink to="/page2">Page2 |</NavLink>
+      <NavLink to="/page3">Page3 |</NavLink>
+      <NavLink to="/page4">Page4 |</NavLink>
+      <NavLink to="/projects">Projects |</NavLink>
+      <NavLink to="/projects/dashboard">Project Dashboard |</NavLink>
+      <NavLink to="/projects/billing">Project Billing</NavLink>
+    </>
+  );
+};
 
-  const activeStyle = { background: "red", color: "white" };
+const AppNavigationNavigate = () => {
+  let navigate = useNavigate();
 
-  let authStatus = state.authstore.value;
-  console.log(authStatus);
-  if (!authStatus) {
-    return (
-      <>
-        <NavLink
-          to="/page1"
-          style={({ isActive }) => (isActive ? activeStyle : null)}
-        >
-          Page 1 |
-        </NavLink>
-        <NavLink
-          to="/page2"
-          style={({ isActive }) => (isActive ? activeStyle : null)}
-        >
-          Page 2
-        </NavLink>
-      </>
-    );
-  }
+  const go2page = (page) => {
+    navigate(`/${page}`, { replace: true });
+  };
 
   return (
     <>
-      <NavLink
-        to="/page2"
-        style={({ isActive }) => (isActive ? activeStyle : null)}
-      >
-        Page 2 |{" "}
-      </NavLink>
-      <NavLink
-        to="/page3"
-        style={({ isActive }) => (isActive ? activeStyle : null)}
-      >
-        Page 3
-      </NavLink>
+      <input type="button" value="Page1" onClick={() => go2page("page1")} />
+      <input type="button" value="Page2" onClick={() => go2page("page2")} />
+      <input type="button" value="Page3" onClick={() => go2page("page3")} />
+      <input type="button" value="Page4" onClick={() => go2page("page4")} />
     </>
   );
 };
