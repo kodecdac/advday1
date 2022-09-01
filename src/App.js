ec12219ea51b874bc6ed12c4563acc9958b65049
santoshmondal <santosh.ece06@gmail.com>
2022-09-01 17:00:10 +0530

    with redux.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <AppNavigation />
 
       <Routes>
-        <Route index path="/" element={<Page1 />} />
+        <Route path="/" element={<Page1 />} />
         <Route path="/page1" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
         <Route path="/page3" element={<Page3 />} />
