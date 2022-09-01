a0e4eefee1d2e14e05d45b67270845620f79f625
santoshmondal <santosh.ece06@gmail.com>
2022-09-01 16:48:46 +0530

    t6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { NavLink, Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
   return (
@@ -6,17 +6,62 @@ function App() {
       <AppNavigation />
 
       <Routes>
-        <Route index path="/" element={<h1>Page1</h1>} />
-        <Route path="/page1" element={<h1>Page1</h1>} />
-        <Route path="/page2" element={<h1>Page2</h1>} />
-        <Route path="/page3" element={<h1>Page3</h1>} />
+        <Route index path="/" element={<Page1 />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
       </Routes>
     </>
   );
 }
 
+const Page1 = () => {
+  return (
+    <>
+      <h1>Page1</h1>
+    </>
+  );
+};
+
+const Page2 = () => {
+  return (
+    <>
+      <h1>Page2</h1>
+    </>
+  );
+};
+
+const Page3 = () => {
+  return (
+    <>
+      <h1>Page3</h1>
+    </>
+  );
+};
+
 const AppNavigation = () => {
   const activeStyle = { background: "red", color: "white" };
+
+  let authStatus = window.localStorage.getItem("authStatus");
+  console.log(authStatus);
+  if (!authStatus) {
+    return (
+      <>
+        <NavLink
+          to="/page1"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Page 1 |
+        </NavLink>
+        <NavLink
+          to="/page2"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Page 2
+        </NavLink>
+      </>
+    );
+  }
 
   return (
     <>
