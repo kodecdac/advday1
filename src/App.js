b311c93c817b1074814dc0b0d98a9924b88a8fb4
santoshmondal <santosh.ece06@gmail.com>
2022-09-08 11:44:57 +0530

    tddd.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 function App() {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>Learn React Testing</h1>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter Username"
+        name="username"
+        value={user.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        className="form-control"
+        placeholder="Enter Password"
+        name="password"
+        value={user.password}
+        onChange={handleChange}
+      />
+      <input
+        type="button"
+        value="Submit"
+        disabled={!user.username || !user.password}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,64 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("expect username placeholder in the document", () => {
+  render(<App />);
+
+  const usernameElement = screen.getByPlaceholderText("Enter Username");
+  expect(usernameElement).toBeInTheDocument();
+});
+
+test("expect password placeholder in the document", () => {
+  render(<App />);
+
+  const passwordElement = screen.getByPlaceholderText("Enter Password");
+  expect(passwordElement).toBeInTheDocument();
+});
+
+test("expect button to be disabled during initial render", async () => {
+  render(<App />);
+
+  const btnElement = await screen.findByRole("button");
+  expect(btnElement).toBeDisabled();
+});
+
+test("Testing Onchange Username", () => {
+  render(<App />);
+
+  const usernameElement = screen.getByPlaceholderText("Enter Username");
+
+  // by event we are setting the value rohit
+  fireEvent.change(usernameElement, { target: { value: "rohit" } });
+
+  expect(usernameElement.value).toBe("rohit");
+});
+
+test("Testing Onchange Password", () => {
+  render(<App />);
+
+  const passwordElement = screen.getByPlaceholderText("Enter Password");
+
+  // by event we are setting the value rohit
+  fireEvent.change(passwordElement, { target: { value: "adsfadf" } });
+
+  expect(passwordElement.value).toBe("adsfadf");
+});
+
+test("Sumbit should be enabled when there is Username and Password Value", () => {
+  render(<App />);
+
+  const usernameElement = screen.getByPlaceholderText("Enter Username");
+  fireEvent.change(usernameElement, { target: { value: "rohit" } });
+
+  const passwordElement = screen.getByPlaceholderText("Enter Password");
+  fireEvent.change(passwordElement, { target: { value: "rohit" } });
+
+  const btnElement = screen.getByRole("button");
+  expect(btnElement).not.toBeDisabled();
 });
